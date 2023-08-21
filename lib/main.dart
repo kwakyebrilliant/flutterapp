14e47fa2bff6b33f28ba7bef83e4aac8497a3587
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-08-21 20:28:55 +0000

    appbar added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(),
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('my first app'),
+      ),
+    ),
   ));
 }
